753ff95680e2052f877347bc228e9c7c3260ff32
cha1ton <zenschal1@gmail.com>
2025-12-07 02:44:58 -0500

    Actualizar Next.js a la última versión segura

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh1.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh2.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh4.googleusercontent.com",
-      },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "lh1.googleusercontent.com" },
+      { protocol: "https", hostname: "lh2.googleusercontent.com" },
+      { protocol: "https", hostname: "lh4.googleusercontent.com" },
     ],
   },
 };
